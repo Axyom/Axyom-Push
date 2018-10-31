ecbe8b2b0b6601008d03b4547f02ed9377158c56
Axyom <paul.michelon.usach@gmail.com>
2018-10-31 15:36:07 -0400

    Creation of midi version, grab and release functions

--- axyom_push.js
+++ axyom_push.js
@@ -25,38 +25,55 @@ function log() {
   post("\n");
 }
 
-var api;
+var push;
+var selectButtons;
+var stateButtons;
 
 // initialisations
 function bang() {
-    api = new LiveAPI("live_app control_surfaces 0");
-    //log('push path:', api.path);
-    // do stuff with the Device object...
-    //log(api.call("get_control_names"));
-    Track_State_Buttons = api.call("get_control", "Track_State_Buttons");
-    api.call("grab_control", Track_State_Buttons);
-    Track_Select_Buttons = api.call("get_control", "Track_Select_Buttons");
-    api.call("grab_control", Track_Select_Buttons);
+    push = new LiveAPI("live_app control_surfaces 0");
+    selectButtons = new Array();
+    stateButtons = new Array();
     
-    Track_Select_Buttons_Object = new LiveAPI("live_app control_surfaces 0 controls 45");
-    log(Track_Select_Buttons_Object.info);
-    Track_Select_Buttons_Object.call("set_light", "25");
+    // take control of select and state buttons
+    Track_State_Buttons = push.call("get_control", "Track_State_Buttons");
+    Track_Select_Buttons = push.call("get_control", "Track_Select_Buttons");
     
+    // store de api for the select and state buttons
+    for(var i=0; i<8; i++)
+    {
+        selectButtons.push(new LiveAPI("live_app control_surfaces 0 controls "+ (i + 44)));
+        stateButtons.push(new LiveAPI("live_app control_surfaces 0 controls "+ (i + 44 + 9)));
+    }
     
-    //api.call("release_control", Track_State_Buttons);
-    //api.call("release_control", Track_Select_Buttons);
     var foo = new LiveAPI(callback);
 	foo.path = "live_set tracks 2 mixer_device volume"
 	foo.property = "value";
 }
-
-// notes:
-// 52 : track selection
 
 
 function callback(args)
 {
 	outlet(0,args);
     log(args);
+}
+
+function release()
+{
+    push.call("release_control", Track_State_Buttons);
+    push.call("release_control", Track_Select_Buttons);
+}
+
+function grab()
+{
+    push.call("grab_control", Track_State_Buttons);
+    push.call("grab_control", Track_Select_Buttons);
+
+    // default light
+    for(var i=0; i<8; i++)
+    {
+        selectButtons[i].call("set_light", "99");
+        stateButtons[i].call("set_light", "99");
+    }
 }
 
