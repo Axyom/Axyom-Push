442a6e6a68a7e25c34ca19d8c5f2b83025a904a9
Axyom <paul.michelon.usach@gmail.com>
2018-10-31 19:12:58 -0400

    Function Toggle LEDs OK

--- axyom_push.js
+++ axyom_push.js
@@ -1,3 +1,6 @@
+inlets = 1;
+outlets = 1;
+
 function log() {
   for(var i=0,len=arguments.length; i<len; i++) {
     var message = arguments[i];
@@ -25,12 +28,45 @@ function log() {
   post("\n");
 }
 
+// classes
+//--------------------------------------------------------------------
+// Note class
+ 
+function Button(liveObject) 
+{
+    this.object = liveObject; // reference to the liveObject
+    this.state = 0; // On/Off
+}
+ 
+Button.prototype.set_light = function(val) 
+{
+    this.object.call("set_light", val);
+}
+
+Button.prototype.toggle = function() 
+{
+    if(this.state == 0)
+    {
+        this.object.call("set_light", 1);
+        this.state = 1;
+    }
+    else
+    {
+        this.object.call("set_light", 0);
+        this.state = 0;
+    }
+}
+//--------------------------------------------------------------------
+
+// variables globales
+
 var push;
 var selectButtons;
 var stateButtons;
 
 // initialisations
-function bang() {
+function bang() 
+{
     push = new LiveAPI("live_app control_surfaces 0");
     selectButtons = new Array();
     stateButtons = new Array();
@@ -42,8 +78,8 @@ function bang() {
     // store de api for the select and state buttons
     for(var i=0; i<8; i++)
     {
-        selectButtons.push(new LiveAPI("live_app control_surfaces 0 controls "+ (i + 44)));
-        stateButtons.push(new LiveAPI("live_app control_surfaces 0 controls "+ (i + 44 + 9)));
+        selectButtons.push(new Button(new LiveAPI("live_app control_surfaces 0 controls "+ (i + 44))));
+        stateButtons.push(new Button(new LiveAPI("live_app control_surfaces 0 controls "+ (i + 44 + 9))));
     }
     
     var foo = new LiveAPI(callback);
@@ -72,8 +108,28 @@ function grab()
     // default light
     for(var i=0; i<8; i++)
     {
-        selectButtons[i].call("set_light", "99");
-        stateButtons[i].call("set_light", "99");
+        selectButtons[i].set_light("99");
+        stateButtons[i].set_light("99");
     }
 }
 
+function list() // midi cc to buttons
+{
+    log(arguments[0] + " " + arguments[1]);
+
+    if (arguments[1] == 127) // toggle button
+    {
+        var i
+        
+        if (arguments[0] < 100) // select buttons
+        {
+            var i = arguments[0]-20;
+            selectButtons[i].toggle();
+        }
+        else // state buttons
+        {
+            var i = arguments[0]-102;
+            stateButtons[i].toggle();
+        }
+    }
+}
